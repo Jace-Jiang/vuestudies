db1b15ee8ecc41c394661005da50ad846fd59c1c
Jace Jiang <2060841093@qq.com>
2021-04-21 16:03:08 +0800

    提交文件

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <form>
+      <input type="text" v-model="stu.id">
+      <input type="text" v-model="stu.name">
+      <input type="text" v-model="stu.age">
+      <input type="submit" @click="addStu">
+    </form>
+    <ul>
+      <li v-for="(tem,index) in state.stus" :key="tem.id" @click="remStu(index)">{{tem.name}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {reactive, ref} from 'vue'
+export default {
+  name: 'HelloWorld',
+  setup(){
+    let cc=ref(123)
+    console.log(cc.__v_isRef)
+    let state = reactive({
+      stus:[
+        {id:1,name:"zhangsan",age:20},
+        {id:2,name:"lisi",age:22},
+        {id:3,name:"zhangsan",age:23}
+      ]
+    });
+    let stu={id:null,name:"",age:0}
+    function remStu(index){
+      console.log(this)
+      state.stus = state.stus.filter((stu,idx))
+    }
+    return {state,stu,remStu}
+  }
+}
+</script>
---
+++ src/components/Study001.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="tem in stu" :key="tem.id">{{tem.name}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+export default {
+    mane:'App',
+    data:function() {
+        return {
+            stu: [
+                {id:1,name:"zhangsan",age:20},
+                {id:2,name:"lisi",age:21},
+                {id:3,name:"wangwu",age:23}
+            ]
+        }
+    },
+}
+</script>
