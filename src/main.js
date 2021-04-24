a67936e56d0548b139541c04aa705d3425d1523c
Jiangliquan <Jliquan32@163.com>
2021-04-24 12:27:04 +0800

    添加路由跳转

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css'
-
-createApp(App).mount('#app')
+import {createRouter,createWebHashHistory} from "vue-router"
+import home from "./view/home.vue"
+ 
+const VueRouter = createRouter({
+    history:createWebHashHistory(),
+    routes:[
+      {
+        path: '/',
+        component: home
+      }
+    ]
+  })
+const app = createApp(App);
+app.use(VueRouter)
+app.mount('#app')
---
+++ src/view/home.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>{{title}}</div>
+    <ul v-for="item in list" :key="item.id">
+        <li>{{item.text}}</li>
+    </ul>
+</template>
+
+<script>
+
+export default {
+    name:"home",
+
+    data(){
+        return {
+            title:"vue3 studies",
+            list:[
+                {
+                    id:1,
+                    text:"基础"
+                },
+                {
+                    id:2,
+                    text:"深入组件"
+                },
+                {
+                    id:3,
+                    text:"过渡&动画"
+                },
+                {
+                    id:4,
+                    text:"可复用&组合"
+                },
+                {
+                    id:5,
+                    text:"高阶指南"
+                },
+                {
+                    id:6,
+                    text:"工具"
+                },
+                {
+                    id:7,
+                    text:"规模化"
+                },
+                {
+                    id:8,
+                    text:"无障碍"
+                }
+            ]
+        }
+    },
+
+    components:{},
+
+    methods:{
+
+    }
+}
+</script>
