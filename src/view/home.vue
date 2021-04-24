835aa1b608a40714ff7f7b0bb77b2479a476ad19
Jiangliquan <Jliquan32@163.com>
2021-04-24 17:34:00 +0800

    添加eslint和动效superslide插件

--- src/view/home.vue
+++ src/view/home.vue
@@ -1,26 +1,61 @@
 <template>
-    <table>
+    <div class="title">
+        <p>{{title}}</p>
+        <img src="../assets/horse.jpg">
+    </div>
+    <table align="center">
         <tr>
-            <td>
-                <div class="title">
-                    <p>{{title}}</p>
-                    <img src="../assets/horse.jpg">
-                </div>
-            </td>
-            <td>
+            <td valign="top">
                 <p class="demo">基础</p>
                 <ul v-for="item in baseList" :key="item.id">
                     <li @click="linkClick(item.path)">{{item.text}}</li>
                 </ul>
             </td>
-            <td>
+            <td valign="top">
                 <p class="demo">深入组件</p>
                 <ul v-for="item in moduleList" :key="item.id">
                     <li @click="linkClick(item.path)">{{item.text}}</li>
                 </ul>
             </td>
+            <td valign="top">
+                <p class="demo">过渡&动画</p>
+                <ul v-for="item in cartoonList" :key="item.id">
+                    <li @click="linkClick(item.path)">{{item.text}}</li>
+                </ul>
+            </td>
+            <td valign="top">
+                <p class="demo">可复用&组合</p>
+                <ul v-for="item in combinationList" :key="item.id">
+                    <li @click="linkClick(item.path)">{{item.text}}</li>
+                </ul>
+            </td>
+            <td valign="top">
+                <p class="demo">高阶指南</p>
+                <ul v-for="item in highList" :key="item.id">
+                    <li @click="linkClick(item.path)">{{item.text}}</li>
+                </ul>
+            </td>
+            <td valign="top">
+                <p class="demo">工具</p>
+                <ul v-for="item in toolsList" :key="item.id">
+                    <li @click="linkClick(item.path)">{{item.text}}</li>
+                </ul>
+            </td>
+            <td valign="top">
+                <p class="demo">规模化</p>
+                <ul v-for="item in scaleList" :key="item.id">
+                    <li @click="linkClick(item.path)">{{item.text}}</li>
+                </ul>
+            </td>
+            <td valign="top">
+                <p class="demo">无障碍</p>
+                <ul v-for="item in accessibleList" :key="item.id">
+                    <li @click="linkClick(item.path)">{{item.text}}</li>
+                </ul>
+            </td>
         </tr>
     </table>
+  
 </template>
 
 <script>
@@ -30,7 +65,7 @@ export default {
 
     data(){
         return {
-            title:"vue3 studies",
+            title:"重学前端（vue3）",
             baseList:[ 
                 { id:1, text:"介绍", path:"introduce" },
                 { id:2, text:"应用 & 组件实例", path:"application" },
@@ -54,6 +89,42 @@ export default {
                 { id:7, text:"动态组件 & 异步组件", path:"build" },
                 { id:8, text:"模板引用", path:"build" },
                 { id:9, text:"处理边界情况", path:"build" }
+            ],
+            cartoonList:[ 
+                { id:1, text:"过渡 & 动画概述", path:"build" },
+                { id:2, text:"进入过渡 & 离开过渡", path:"build" },
+                { id:3, text:"列表过渡", path:"build" },
+                { id:4, text:"状态过渡", path:"build" }
+            ],
+            combinationList:[
+                { id:1, text:"混入", path:"build" },
+                { id:2, text:"混入", path:"build" },
+                { id:3, text:"Teleport", path:"build" },
+                { id:4, text:"渲染函数", path:"build" },
+                { id:4, text:"插件", path:"build" }
+            ],
+            highList:[
+                { id:1, text:"响应性", path:"build" },
+                { id:2, text:"组合式API", path:"build" },
+                { id:3, text:"渲染机制和优化", path:"build" },
+                { id:4, text:"Vue2中的更改检测警告", path:"build" }
+            ],
+            toolsList:[
+                { id:1, text:"单文件组件", path:"build" },
+                { id:2, text:"测试", path:"build" },
+                { id:3, text:"TypeScript 支持", path:"build" },
+                { id:4, text:"Mobile", path:"build" }
+            ],
+            scaleList:[
+                { id:1, text:"路由", path:"build" },
+                { id:2, text:"状态管理", path:"build" },
+                { id:4, text:"服务端渲染", path:"build" }
+            ],
+            accessibleList:[
+                { id:1, text:"基础", path:"build" },
+                { id:2, text:"语义学", path:"build" },
+                { id:4, text:"标准", path:"build" },
+                { id:4, text:"资源", path:"build" }
             ]
         }
     },
