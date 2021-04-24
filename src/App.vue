6c88797f61f893957afc0bea5710e9ad44bb2227
Jiangliquan <Jliquan32@163.com>
2021-04-24 10:49:04 +0800

    搭建简单的vue3学习项目

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,64 @@
 <template>
   <div id="app">
-    <div>此框内为app.vue中的内容，不会因为点击下面的router-link而消失</div>
-    <router-link v-bind:to="'/hello'">say Hello</router-link>
-    <router-link v-bind:to="'/world'">say World</router-link>
-    <router-view/>
+    <ul>
+      <li v-for="item in links" :key="item.text"><a @click="$goRoute(item.route)">{{item.text}}</a></li>
+    </ul>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-  components: {
+  export default {
+    name: 'app',
+    data () {
+      return {
+        links: [
+          {
+            text: '首页',
+            route: '/home'
+          },
+          {
+            text: '页面1',
+            route: '/page01'
+          },
+          {
+            text: '页面2',
+            route: '/page02'
+          }
+        ]
+      }
+    }
   }
-}
 </script>
+
+<style>
+  #app {
+    width: 100%;
+    height: calc(100vh);
+    background:url("./assets/horse.jpg") no-repeat fixed top;
+  }
+  .text-center {
+    text-align: center;
+  }
+  .spacing {
+    margin-top: 30px;
+  }
+  .red {
+    color: darkred;
+  }
+  ul li {
+    display: inline-block;
+    margin-right: 10px;
+  }
+  ul li a{
+    display: inherit;
+    padding: 5px 10px;
+    border: 1px solid #ccc;
+  }
+  ul li a:hover{
+    cursor: pointer;
+    color: #fff;
+    background-color: #138bec;
+    border: 1px solid #138bec;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
-import rootApp from '@/view/rootApp'
-import hello from '@/view/hello'
-import world from '@/view/world'
+import home from '@/components/home'
+import page01 from '@/components/page01'
+import page02 from '@/components/page02'
+import page01A from '@/components/page01/page01-A'
+import page01B from '@/components/page01/page01-b'
+import pageEnd from '@/components/page01/B/end'
 
 Vue.use(Router)
 
@@ -11,13 +13,42 @@ export default new Router({
   routes: [
     {
       path: '/',
-      component: rootApp
-    },{
-      path: '/hello',
-      component: hello
-    },{
-      path: '/world',
-      component: world
+      name: 'home',
+      component: home
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: home
+    },
+    {
+      path: '/page01',
+      name: 'page01',
+      component: page01,
+      children: [
+        {
+          path: 'page01-a',
+          name: 'page01A',
+          component: page01A
+        },
+        {
+          path: 'page01-b',
+          name: 'page01B',
+          component: page01B,
+          children: [
+            {
+              path: 'end',
+              name: 'pageEnd',
+              component: pageEnd
+            }
+          ]
+        }
+      ]
+    },
+    {
+      path: '/page02',
+      name: 'page02',
+      component: page02
     }
   ]
 })
