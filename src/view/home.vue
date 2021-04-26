27435b93eb753a8944ddab7a640d16359a60a538
Jiangliquan <Jliquan32@163.com>
2021-04-26 10:39:45 +0800

    首页的左侧菜单改成手风琴动效

--- src/view/home.vue
+++ src/view/home.vue
@@ -1,61 +1,20 @@
 <template>
-    <div class="title">
-        <p>{{title}}</p>
+    <div class="content">
+        <div class="menu">
+            <ul>
+                <!-- 循环数据在点击调用changeli方法时将当前索引和本条数据传进去,并使用当前数据show的bool值添加或移除样式 -->
+                <li :class="[{active:item.show}]" @click="changeli(index,item)" v-for="(item,index) in headerData" :key="item.id">
+                    {{item.name}}
+                    <!-- 判断当前这条数据的bool值如果是true就打开二级菜单,如果是false就关闭二级菜单 -->
+                    <ul v-show="item.show"> 
+                        <!-- 循环二级菜单数据并使用.stop阻止冒泡 -->
+                        <li v-for="a in item.list" v-on:click.stop="linkMenu(a.path)" :key="a.id">{{a.text}}</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
         <img src="../assets/horse.jpg">
     </div>
-    <table align="center">
-        <tr>
-            <td valign="top">
-                <p class="demo">基础</p>
-                <ul v-for="item in baseList" :key="item.id">
-                    <li @click="linkClick(item.path)">{{item.text}}</li>
-                </ul>
-            </td>
-            <td valign="top">
-                <p class="demo">深入组件</p>
-                <ul v-for="item in moduleList" :key="item.id">
-                    <li @click="linkClick(item.path)">{{item.text}}</li>
-                </ul>
-            </td>
-            <td valign="top">
-                <p class="demo">过渡&动画</p>
-                <ul v-for="item in cartoonList" :key="item.id">
-                    <li @click="linkClick(item.path)">{{item.text}}</li>
-                </ul>
-            </td>
-            <td valign="top">
-                <p class="demo">可复用&组合</p>
-                <ul v-for="item in combinationList" :key="item.id">
-                    <li @click="linkClick(item.path)">{{item.text}}</li>
-                </ul>
-            </td>
-            <td valign="top">
-                <p class="demo">高阶指南</p>
-                <ul v-for="item in highList" :key="item.id">
-                    <li @click="linkClick(item.path)">{{item.text}}</li>
-                </ul>
-            </td>
-            <td valign="top">
-                <p class="demo">工具</p>
-                <ul v-for="item in toolsList" :key="item.id">
-                    <li @click="linkClick(item.path)">{{item.text}}</li>
-                </ul>
-            </td>
-            <td valign="top">
-                <p class="demo">规模化</p>
-                <ul v-for="item in scaleList" :key="item.id">
-                    <li @click="linkClick(item.path)">{{item.text}}</li>
-                </ul>
-            </td>
-            <td valign="top">
-                <p class="demo">无障碍</p>
-                <ul v-for="item in accessibleList" :key="item.id">
-                    <li @click="linkClick(item.path)">{{item.text}}</li>
-                </ul>
-            </td>
-        </tr>
-    </table>
-  
 </template>
 
 <script>
@@ -65,74 +24,117 @@ export default {
 
     data(){
         return {
-            title:"重学前端（vue3）",
-            baseList:[ 
-                { id:1, text:"介绍", path:"introduce" },
-                { id:2, text:"应用 & 组件实例", path:"application" },
-                { id:3, text:"模板语法", path:"build" },
-                { id:4, text:"Data Property 和方法", path:"build" },
-                { id:5, text:"计算属性和侦听器", path:"build" },
-                { id:6, text:"Class 与 Style 绑定", path:"build" },
-                { id:7, text:"条件渲染", path:"build" },
-                { id:8, text:"列表渲染", path:"build" },
-                { id:9, text:"事件处理", path:"build" },
-                { id:10, text:"表单输入绑定", path:"build" },
-                { id:11, text:"组件基础", path:"build" }
-            ],
-            moduleList:[ 
-                { id:1, text:"组件注册", path:"build" },
-                { id:2, text:"Props", path:"build" },
-                { id:3, text:"非 Prop 的 Attribute", path:"build" },
-                { id:4, text:"自定义事件", path:"build" },
-                { id:5, text:"插槽", path:"build" },
-                { id:6, text:"提供 / 注入", path:"build" },
-                { id:7, text:"动态组件 & 异步组件", path:"build" },
-                { id:8, text:"模板引用", path:"build" },
-                { id:9, text:"处理边界情况", path:"build" }
-            ],
-            cartoonList:[ 
-                { id:1, text:"过渡 & 动画概述", path:"build" },
-                { id:2, text:"进入过渡 & 离开过渡", path:"build" },
-                { id:3, text:"列表过渡", path:"build" },
-                { id:4, text:"状态过渡", path:"build" }
-            ],
-            combinationList:[
-                { id:1, text:"混入", path:"build" },
-                { id:2, text:"混入", path:"build" },
-                { id:3, text:"Teleport", path:"build" },
-                { id:4, text:"渲染函数", path:"build" },
-                { id:4, text:"插件", path:"build" }
-            ],
-            highList:[
-                { id:1, text:"响应性", path:"build" },
-                { id:2, text:"组合式API", path:"build" },
-                { id:3, text:"渲染机制和优化", path:"build" },
-                { id:4, text:"Vue2中的更改检测警告", path:"build" }
-            ],
-            toolsList:[
-                { id:1, text:"单文件组件", path:"build" },
-                { id:2, text:"测试", path:"build" },
-                { id:3, text:"TypeScript 支持", path:"build" },
-                { id:4, text:"Mobile", path:"build" }
-            ],
-            scaleList:[
-                { id:1, text:"路由", path:"build" },
-                { id:2, text:"状态管理", path:"build" },
-                { id:4, text:"服务端渲染", path:"build" }
-            ],
-            accessibleList:[
-                { id:1, text:"基础", path:"build" },
-                { id:2, text:"语义学", path:"build" },
-                { id:4, text:"标准", path:"build" },
-                { id:4, text:"资源", path:"build" }
-            ]
+            headerData: [{
+                    id:1,
+                    name: '基础',
+                    list: [
+                        { id:1, text:"介绍", path:"introduce" },
+                        { id:2, text:"应用 & 组件实例", path:"application" },
+                        { id:3, text:"模板语法", path:"build" },
+                        { id:4, text:"Data Property 和方法", path:"build" },
+                        { id:5, text:"计算属性和侦听器", path:"build" },
+                        { id:6, text:"Class 与 Style 绑定", path:"build" },
+                        { id:7, text:"条件渲染", path:"build" },
+                        { id:8, text:"列表渲染", path:"build" },
+                        { id:9, text:"事件处理", path:"build" },
+                        { id:10, text:"表单输入绑定", path:"build" },
+                        { id:11, text:"组件基础", path:"build" }
+                    ],
+                    show: false
+                }, {
+                    id:2,
+                    name: '深入组件',
+                    list: [
+                        { id:1, text:"组件注册", path:"build" },
+                        { id:2, text:"Props", path:"build" },
+                        { id:3, text:"非 Prop 的 Attribute", path:"build" },
+                        { id:4, text:"自定义事件", path:"build" },
+                        { id:5, text:"插槽", path:"build" },
+                        { id:6, text:"提供 / 注入", path:"build" },
+                        { id:7, text:"动态组件 & 异步组件", path:"build" },
+                        { id:8, text:"模板引用", path:"build" },
+                        { id:9, text:"处理边界情况", path:"build" }
+                    ],
+                    show: false
+                }, {
+                    id:3,
+                    name: '过渡&动画',
+                    list: [
+                        { id:1, text:"过渡 & 动画概述", path:"build" },
+                        { id:2, text:"进入过渡 & 离开过渡", path:"build" },
+                        { id:3, text:"列表过渡", path:"build" },
+                        { id:4, text:"状态过渡", path:"build" }
+                    ],
+                    show: false
+                }, {
+                    id:4,
+                    name: '可复用&组合',
+                    list: [
+                        { id:1, text:"混入", path:"build" },
+                        { id:2, text:"混入", path:"build" },
+                        { id:3, text:"Teleport", path:"build" },
+                        { id:4, text:"渲染函数", path:"build" },
+                        { id:4, text:"插件", path:"build" }
+                    ],
+                    show: false
+                }, {
+                    id:5,
+                    name: '高阶指南',
+                    list: [
+                        { id:1, text:"响应性", path:"build" },
+                        { id:2, text:"组合式API", path:"build" },
+                        { id:3, text:"渲染机制和优化", path:"build" },
+                        { id:4, text:"Vue2中的更改检测警告", path:"build" }
+                    ],
+                    show: false
+                }, {
+                    id:5,
+                    name: '工具',
+                    list: [
+                        { id:1, text:"单文件组件", path:"build" },
+                        { id:2, text:"测试", path:"build" },
+                        { id:3, text:"TypeScript 支持", path:"build" },
+                        { id:4, text:"Mobile", path:"build" }
+                    ],
+                    show: false
+                }, {
+                    id:5,
+                    name: '规模化',
+                    list: [
+                        { id:1, text:"路由", path:"build" },
+                        { id:2, text:"状态管理", path:"build" },
+                        { id:4, text:"服务端渲染", path:"build" }
+                    ],
+                    show: false
+                }, {
+                    id:5,
+                    name: '无障碍',
+                    list: [
+                        { id:1, text:"基础", path:"build" },
+                        { id:2, text:"语义学", path:"build" },
+                        { id:4, text:"标准", path:"build" },
+                        { id:4, text:"资源", path:"build" }
+                    ],
+                    show: false
+                }]
         }
     },
 
     components:{},
 
     methods:{
-        linkClick(route){
+        changeli: function(ind, item) {
+            // 先循环数据中的show将其全部置为false,此时模板里的v-if判断生效关闭全部二级菜单,并移除样式
+            this.headerData.forEach(i => {
+                // 判断如果数据中的headerData[i]的show属性不等于当前数据的show属性那么headerData[i]等于false
+                if (i.show !== this.headerData[ind].show) {
+                    i.show = false;
+                };
+            });
+            // 取反(true或false)
+            item.show = !item.show;
+        },
+        linkMenu: function(route) {
             this.$router.push(route)
         }
     }
@@ -140,26 +142,45 @@ export default {
 </script>
 
 <style lang="less" scoped>
-    .title {
-        text-align: center;
-        p {
-            font-size: 32px;
-            font-weight: bold;
-            color: rgb(145, 16, 16);
+    .content {
+        img {
+            margin-left: 5px;
+            float:left;
+        }
+        .menu {
+            float: left;
+            margin-left: 5px;
+            width: 390px;
+            background-color: #51add8;
+            color: #ffffff;
+            >ul {
+                width: 100%;
+                @include clearfix;
+                >li {
+                    width: 88%;
+                    border: 1px solid #ffffff;
+                    cursor: pointer; // float: left;
+                    color: 20px;
+                    text-align: center;
+                    line-height: 60px;
+                    &:hover {
+                        background-color: #ff9800;
+                    }
+                    >ul {
+                        width: 86%;
+                        background: rgb(72, 175, 132);
+                        li{
+                            &:hover{
+                                background: #bfd851;
+                            }
+                        }
+                    }
+                }
+                .active {
+                    background-color: #ff9800;
+                }
+            }
         }
     }
-    .demo {
-        font-size: 23px;
-        font-weight: bold;
-        padding-left: 23px;
-        color: rgb(145, 16, 16);
-    }
-    ul {
-        width: 170px;
-        li {
-            cursor: pointer;
-            font-weight: bold;
-            color:rgb(45, 141, 185);
-        }
-    }
+
 </style>
